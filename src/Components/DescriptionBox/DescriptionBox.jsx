8efe0fa0ba7d46aa2145bbf712386d170325aa06
cardinaly <119264263+cardinaly@users.noreply.github.com>
2024-04-14 14:20:25 +0300

    e-commerce source

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./DescriptionBox.css"
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+          <div className="descriptionbox-nav-box">Description</div>
+          <div className="descriptionbox-nav-box fade">Reviews (122) </div>
+        </div>
+        <div className="descriptionbox-description">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio quas eveniet praesentium adipisci aliquam quos, eius rem obcaecati labore. Debitis, corporis cumque. Aperiam, esse. Eligendi, debitis! Similique, voluptate quis. Cupiditate animi aliquam impedit hic placeat saepe consequuntur, illum deserunt perspiciatis, culpa modi possimus ducimus natus sapiente accusantium vitae dolore tempore.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita doloremque nobis eveniet aliquid tempora consequuntur molestias odit accusantium, corrupti voluptatum?
+          </p>
+        </div>
+    </div>
+  )
+}
